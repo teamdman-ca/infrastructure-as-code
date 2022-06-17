3844d944c6cacfae3ca384102e53deeca588960a
TeamDman <TeamDman9201@gmail.com>
2022-06-17 18:41:47 -0400

    update azurerm provider

--- ca-teamdman/boilerplate.tf
+++ ca-teamdman/boilerplate.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = ">=3.10.0"
     }
   }
 }
@@ -16,4 +16,8 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+}
+
+data "azurerm_client_config" "current" {
+  
 }
