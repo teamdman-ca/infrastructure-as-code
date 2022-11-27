4936e05fc8bc36975563ec982dc83b42d5f9a725
TeamDman <TeamDman9201@gmail.com>
2022-11-27 00:47:08 -0500

    add cert manager

--- ca.teamdman.cluster/kubernetes/cert-manager/install.ps1
+++ ca.teamdman.cluster/kubernetes/cert-manager/install.ps1
@@ -1,9 +1,13 @@
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-helm install cert-manager jetstack/cert-manager `
-    --namespace "cert-manager" `
-    --create-namespace `
-    --version "v1.8.2" `
-    -f "values.yaml"
+#!/usr/bin/pwsh
 
-# kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.yaml
+kubectl kustomize --enable-helm | kubectl apply -f -
+
+# helm repo add jetstack https://charts.jetstack.io
+# helm repo update
+# helm install cert-manager jetstack/cert-manager `
+#     --namespace "cert-manager" `
+#     --create-namespace `
+#     --version "v1.8.2" `
+#     -f "values.yaml"
+
+# # kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.yaml
--- ca.teamdman.cluster/kubernetes/cert-manager/uninstall.ps1
+++ ca.teamdman.cluster/kubernetes/cert-manager/uninstall.ps1
@@ -1,3 +1,7 @@
-helm --namespace cert-manager delete cert-manager
-kubectl delete namespace cert-manager
-# kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.yaml
+#!/usr/bin/pwsh
+
+kubectl kustomize --enable-helm | kubectl delete -f -
+
+# helm --namespace cert-manager delete cert-manager
+# kubectl delete namespace cert-manager
+# # kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.yaml
