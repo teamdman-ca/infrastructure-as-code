6bc9deaf79e26848e09206b155334026227c2963
TeamDman <TeamDman9201@gmail.com>
2022-11-27 15:01:41 -0500

    attempt fix on finalizers showing up on apps

--- ca.teamdman.cluster/kubernetes/app-of-apps/uninstall.ps1
+++ ca.teamdman.cluster/kubernetes/app-of-apps/uninstall.ps1
@@ -4,6 +4,10 @@
 # for now we can just hard remove them before nuking the resources
 kubectl patch app argocd -n argocd -p '{"metadata":{"finalizers":null}}' --type=merge
 kubectl patch app app-of-apps -n argocd -p '{"metadata":{"finalizers":null}}' --type=merge
+kubectl patch app blob-csi-driver -n argocd -p '{"metadata":{"finalizers":null}}' --type=merge
+kubectl patch app cert-manager -n argocd -p '{"metadata":{"finalizers":null}}' --type=merge
+kubectl patch app external-secrets -n argocd -p '{"metadata":{"finalizers":null}}' --type=merge
+kubectl patch app ingress-nginx -n argocd -p '{"metadata":{"finalizers":null}}' --type=merge
 kubectl patch appproject core -n argocd -p '{"metadata":{"finalizers":null}}' --type=merge
 
 kubectl delete -k .
--- ca.teamdman.cluster/kubernetes/argocd/install.ps1
+++ ca.teamdman.cluster/kubernetes/argocd/install.ps1
@@ -2,8 +2,16 @@
 kubectl create ns argocd
 kubectl apply -k .
 
+Write-Host "Waiting for initial secret to become available" -ForegroundColor "Cyan"
+while ($true) {
+    $x = kubectl get secret -n argocd argocd-initial-admin-secret -o name
+    if ($x.Count -gt 0) {
+        break
+    }
+}
+
 $pw = kubectl get secret -n argocd argocd-initial-admin-secret -o json | ConvertFrom-Json
 $pw = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($pw.data.password))
 Write-Host "ArgoCD admin password is `"${pw}`""
 
-Write-Host "Waiting for certificate request to be approved"
+Write-Host "Waiting for certificate request to be approved" -ForegroundColor "Cyan"
