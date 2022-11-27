ebef15b32ecfe8cd938dc72970e330ff8957c291
TeamDman <TeamDman9201@gmail.com>
2022-11-27 16:19:14 -0500

    clean up nginx install

--- kubernetes/ingress-nginx/install.ps1
+++ kubernetes/ingress-nginx/install.ps1
@@ -1,8 +1,3 @@
 #!/usr/bin/pwsh
 kubectl create ns ingress-nginx
 kubectl kustomize --enable-helm | kubectl apply -f -
-
-# helm install ingress-nginx ingress-nginx/ingress-nginx `
-#     --namespace ingress-basic `
-#     --create-namespace `
-#     --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz
--- kubernetes/ingress-nginx/uninstall.ps1
+++ kubernetes/ingress-nginx/uninstall.ps1
@@ -1,5 +1,2 @@
 kubectl kustomize --enable-helm | kubectl delete -f -
-kubectl delete ns nginx-ingress
-
-# helm --namespace ingress-basic delete ingress-nginx
-# kubectl delete namespace ingress-basic
+kubectl delete ns ingress-nginx
