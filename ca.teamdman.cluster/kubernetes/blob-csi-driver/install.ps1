80503ed7fbc9e02739e55434a38e8350a445d407
TeamDman <TeamDman9201@gmail.com>
2022-11-27 00:31:52 -0500

    add blob csi driver

--- ca.teamdman.cluster/kubernetes/blob-csi-driver/install.ps1
+++ ca.teamdman.cluster/kubernetes/blob-csi-driver/install.ps1
@@ -1,1 +1,4 @@
-helm install blob-csi-driver blob-csi-driver/blob-csi-driver --set node.enableBlobfuseProxy=true --namespace kube-system --set cloud=AzureStackCloud
+#!/usr/bin/pwsh
+
+kubectl kustomize --enable-helm | kubectl apply -f -
+# helm install blob-csi-driver blob-csi-driver/blob-csi-driver --set node.enableBlobfuseProxy=true --namespace kube-system --set cloud=AzureStackCloud
--- ca.teamdman.cluster/kubernetes/blob-csi-driver/uninstall.ps1
+++ ca.teamdman.cluster/kubernetes/blob-csi-driver/uninstall.ps1
@@ -1,1 +1,4 @@
-helm uninstall --namespace kube-system blob-csi-driver
+#!/usr/bin/pwsh
+
+kubectl kustomize --enable-helm | kubectl delete -f -
+# helm uninstall --namespace kube-system blob-csi-driver
