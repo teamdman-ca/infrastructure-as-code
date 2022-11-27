84152c0ce5c826bd309c146248e8b88389ed11dc
TeamDman <TeamDman9201@gmail.com>
2022-11-27 00:22:49 -0500

    fix certs

--- ca.teamdman.cluster/kubernetes/app-of-apps/uninstall.ps1
+++ ca.teamdman.cluster/kubernetes/app-of-apps/uninstall.ps1
@@ -1,2 +1,9 @@
 #!/usr/bin/pwsh
+
+# something keeps adding the finalizers back and I don't know why
+# for now we can just hard remove them before nuking the resources
+kubectl patch app argocd -n argocd -p '{"metadata":{"finalizers":null}}' --type=merge
+kubectl patch app app-of-apps -n argocd -p '{"metadata":{"finalizers":null}}' --type=merge
+kubectl patch appproject core -n argocd -p '{"metadata":{"finalizers":null}}' --type=merge
+
 kubectl delete -k .
--- ca.teamdman.cluster/kubernetes/argocd/install.ps1
+++ ca.teamdman.cluster/kubernetes/argocd/install.ps1
@@ -1,3 +1,9 @@
 #!/usr/bin/pwsh
 kubectl create ns argocd
 kubectl apply -k .
+
+$pw = kubectl get secret -n argocd argocd-initial-admin-secret -o json | ConvertFrom-Json
+$pw = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($pw.data.password))
+Write-Host "ArgoCD admin password is `"${pw}`""
+
+Write-Host "Waiting for certificate request to be approved"
